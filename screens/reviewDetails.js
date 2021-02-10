fc67a8202c83be0ad1353997feec2764a47a2805
rajeevbhosle <rajeev.bhosle.222@gmail.com>
2021-02-10 12:23:40 +0530

    Inital commit

---
+++ screens/reviewDetails.js
@@ -0,0 +1,42 @@
+
+
+import React from 'react';
+import { StyleSheet, View, Text, Image, Button } from 'react-native';
+import { globalStyles, images } from '../styles/global';
+import Card from '../shared/card';
+
+export default function ReviewDetails({ navigation }) {
+    const rating = navigation.getParam('rating');
+
+    const goback = () => {
+        navigation.navigate('Home');
+    }
+
+    return (
+        <View style={globalStyles.container}>
+            <Card>
+                <Text style={globalStyles.titleText}>
+                    {navigation.getParam('title')}
+                </Text>
+                <Text>{navigation.getParam('body')}</Text>
+                <View style={styles.rating}>
+                    <Text>GameZone rating: </Text>
+                    <Image source={images.ratings[rating]} />
+                </View>
+            </Card>
+            <Button title='go back' onPress={goback}> </Button>
+
+        </View>
+    );
+}
+
+const styles = StyleSheet.create({
+    rating: {
+        flexDirection: 'row',
+        justifyContent: 'center',
+        paddingTop: 16,
+        marginTop: 16,
+        borderTopWidth: 1,
+        borderTopColor: '#eee',
+    }
+});
